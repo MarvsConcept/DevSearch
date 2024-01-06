38e592ccc8b5050af13173cb63e1246c6abacd1e
MarvsConcept <marvade8@gmail.com>
2024-01-06 09:03:14 +0100

    update

--- users/signals.py
+++ users/signals.py
@@ -4,21 +4,22 @@ from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
     
-# @receiver(post_save, sender=Profile)
+# @receiver(post_save, sender=Profile) 
 def createProfile(sender, instance, created, **kwargs):
-    print('Profile signal trigger')
+    print('Profile signal triggered')
     if created:
         user = instance
         profile = Profile.objects.create(
             user=user,
             username=user.username,
-            email=user.first_name,
-            )
+            email=user.email,
+            name=user.first_name,
+        )
         
 def deleteUser(sender, instance, **kwargs):
     user = instance.user
     user.delete()
-    print('Deleting user...')    
+     
     
-    post_save.connect(createProfile, sender=User)
-    post_delete.connect(deleteUser, sender=Profile)
+post_save.connect(createProfile, sender=User)
+post_delete.connect(deleteUser, sender=Profile)
