b71f19f9fa0471c7e69b81443b9e545572656e14
MarvsConcept <marvade8@gmail.com>
2024-01-08 18:20:51 +0100

    update

--- projects/urls.py
+++ projects/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.projects, name="projects"),
     path('project/<str:pk>/', views.project, name="project"),
     path('create-project', views.CreateProject, name='create-project'),
-    path('update-project/<str:pk>', views.UpdateProject, name='update-project'),
-    path('delete-project/<str:pk>', views.DeleteProject, name='delete-project')
+    path('update-project/<str:pk>/', views.UpdateProject, name='update-project'),
+    path('delete-project/<str:pk>/', views.DeleteProject, name='delete-project')
 ]
--- users/views.py
+++ users/views.py
@@ -2,10 +2,11 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from .models import Profile
+from .forms import CustomUserCreationForm, ProfileForm
 # Create your views here.
+
 
 def loginUser(request):
     page = 'login'
@@ -34,15 +35,15 @@ def loginUser(request):
 
 def logoutUser(request):
     logout(request)
-    messages.error(request, 'User was logged out!' )
+    messages.info(request, 'User was logged out!' )
     return redirect('login')
 
 def registerUser(request):
     page = 'register'
-    form = UserCreationForm()
+    form = CustomUserCreationForm()
     
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.username = user.username.lower()
@@ -51,7 +52,11 @@ def registerUser(request):
             messages.success(request, 'User account was created successfully')
             
             login(request, user)
-            return redirect('profiles')
+            return redirect('edit-account')
+        
+        else:
+            messages.success(request, 'An error has occured during registration')
+            
     
     context = {'page': page, 'form': form}
     return render(request, 'users/login_register.html', context)
@@ -69,3 +74,28 @@ def UserProfile(request, pk):
     
     context = {'profile': profile, 'topSkills':topSkills, 'otherSkills':otherSkills }
     return render(request, 'users/user-profile.html', context)
+
+@login_required(login_url='login')
+def userAccount(request):
+    profile = request.user.profile
+    
+    skills = profile.skill_set.all()
+    projects = profile.project_set.all()
+    
+    context = {'profile': profile, 'skills': skills, 'projects':projects}
+    return render(request, 'users/account.html', context )
+
+@login_required(login_url='login')
+def editAccount(request):
+    profile = request.user.profile
+    form = ProfileForm(instance=profile)
+    
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, request.FILES, instance=profile)
+        if form.is_valid():
+            form.save()
+            
+            return redirect('account')
+            
+    context = {'form': form}
+    return render(request, 'users/profile_form.html', context)
